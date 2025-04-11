8446a20cf4f43a1b7caa765a101b18743212d11b
Cole Mak <mrcolemak@gmail.com>
2025-04-10 18:56:49 -0700

    seperated out data files and reformatted master info doc

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from google import genai
 
 import numpy as np
 import pandas as pd
+import pickle
 
 from dotenv import load_dotenv
 import os
@@ -14,7 +15,27 @@ from sentence_transformers import SentenceTransformer, util
 
 load_dotenv()
 
-with open("extracted_text.json", "r") as file:
+# model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+
+# user_question = input(str("Question: "))
+
+# embedding = model.encode(user_question)
+
+# with open("file_vectors.pkl", 'rb') as file:
+#     rag_vectors = pickle.load(file)
+
+# similarities = {}
+
+# for vector_name, vector in rag_vectors.items():
+#     embeddings = [vector, embedding]
+#     similarity_score = cosine_similarity(embeddings)#[0][1]
+#     print(similarity_score)
+    
+#     similarities[vector_name] = similarity_score
+
+# print(similarities)
+
+with open("full_info.json", "r") as file:
     qa_data = json.load(file)
 qa_data = qa_data['text']
 
@@ -25,8 +46,6 @@ api_key = os.getenv('API_KEY')
 
 client = genai.Client(api_key=api_key)
 
-model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
-
 num_correct = 0
 
 for i, qa_pair in enumerate(qa_test):
@@ -34,11 +53,11 @@ for i, qa_pair in enumerate(qa_test):
     answer = qa_pair["answer"]
 
     prompt = f"""
-    You are an expert in TFT. Use the following retrieved information to answer the question accurately.
-
-    A few things to keep in mind: When asked about traits, do not include the cost of a champion. Shield and Durability are not the same thing. Do not use champion trait information from previous TFT sets, only use the provided context for traits
+    You are an expert in TFT Set 14. Use the following retrieved information to answer the question accurately.
 
     {qa_data} 
+    
+    Make sure to think before answering the user question.
 
     User Question: {question}
 """
@@ -56,21 +75,13 @@ for i, qa_pair in enumerate(qa_test):
     and
     {response}    
 """
-    # completion = openai_client.chat.completions.create(
-    #     model="nvidia/llama-3.1-nemotron-nano-8b-v1:free",
-    #     messages=[{"role": "user",
-    #     "content": open_ai_prompt}]
-    # )
 
     judge_response = client.models.generate_content(
         model="gemini-2.0-flash", contents=judge_prompt
     ).text
 
-    # open_ai_response = completion.choices[0].message.content
-
-
+    ## Cosine Similarity
     # answers = [answer, response]
-
     # embeddings = model.encode(answers)
     # similarity_score = cosine_similarity(embeddings)[0][1]
     
--- test.py
+++ test.py
@@ -3,3 +3,25 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-MiniLM-L6-v2')
 embedding = model.encode("Test sentence")
 print(embedding.shape)
+
+import pickle
+import json
+
+file_vectors = {
+
+}
+
+files = ["champion_info.json", "synergy_info.json", "item_info.json"]
+for file in files:
+    with open(file, "r") as file:
+        qa_data = json.load(file)
+    cur_data = qa_data['text']
+    cur_file = qa_data['file_name']
+
+    embedding = model.encode(cur_data)
+
+    file_vectors[cur_file] = embedding
+
+with open('file_vectors.pkl','wb') as file:
+    pickle.dump(file_vectors, file)
+
