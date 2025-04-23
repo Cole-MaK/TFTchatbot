fec84cb13e8f6035b6562feda871f367a9b77a97
Cole Mak <mrcolemak@gmail.com>
2025-04-23 11:09:32 -0700

    connected tactics with function calling

--- src/functions.py
+++ src/functions.py
@@ -5,11 +5,18 @@ import json
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
+from dotenv import load_dotenv
+
 from google import genai
 from google.genai.types import FunctionDeclaration, GenerateContentConfig, Part, Tool
 
 import os
 
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
+client = genai.Client(api_key=api_key)
 
 def get_rag_keys(file_list):
     '''
@@ -40,11 +47,12 @@ def get_rag_keys(file_list):
     with open('rag_keys.pkl','wb') as file:
         pickle.dump(file_vectors, file)
 
-def get_rag_file(user_question, key_file):
+def get_rag_file(user_question, key_file, top_n=1):
     '''
     input:
     user_question (str): user prompt
     key_file: dictionary of vector embeddings of key documents
+    top_n (int): number of top rag files that we want to obtain
 
     output:
     return key: name to get document
@@ -66,10 +74,10 @@ def get_rag_file(user_question, key_file):
         similarities[vector_name] = similarity_score
     print(similarities)
     
-    top_2_keys = sorted(similarities, key=similarities.get, reverse=True)[:2]
-    top_2 = [key.split('_')[0] for key in top_2_keys]
-    return top_2
-    # key = max(similarities, key= similarities.get).split('_')[0]
+    top_keys = sorted(similarities, key=similarities.get, reverse=True)[:top_n]
+    keys = [key.split('_')[0] for key in top_keys]
+    return keys
+    key = max(similarities, key= similarities.get).split('_')[0]
     return key
 
 def get_filters(prompt):
@@ -79,9 +87,10 @@ def get_filters(prompt):
     filters (list): a list of 1-3 filters to use to search tactics.tools
 
     '''
-    PROJECT_ID = "cmak-123"
-    LOCATION = "us-west1"
-    client = genai.Client(vertexai=True, project=PROJECT_ID, location=LOCATION)
+
+    champions = "Alistar, Annie, Aphelios, Aurora, Brand, Braum, Chogath, Darius, Draven, Dr. Mundo, Ekko, Elise, Fiddlesticks, Galio, Garen, Gragas, Graves, Illaoi, Jarvan IV, Jax, Jhin, Jinx, Kindred, Kobuko, Kogmaw, Leblanc, Leona, Mis Fortune, Mordekaiser, Morgana, Naafiri, Neeko, Nidalee, Poppy, Renekton, Rengar, Rhaast, Samira, Sejuani, Senna, Seraphine, Shaco, Shyvana, Skarner, Sylas, Twisted Fate, Urgot, Varus, Vayne, Veigar, Vex, Vi, Viego, Xayah, Yuumi, Zac, Zed, Zeri, Ziggs, Zyra"
+
+    traits = "3 Anima Squad, 5 Anima Squad, 7 Anima Squad, 10 Anima Squad, 2 BoomBot, 4 BoomBot, 6 BoomBot, 2 Cyberboss, 3 Cyberboss, 4 Cyberboss, 3 Cypher, 4 Cypher, 5 Cypher, 1 Divinicorp, 2 Divinicorp, 3 Divinicorp, 4 Divinicorp, 5 Divinicorp, 6 Divinicorp, 7 Divinicorp, 3 Exotech, 5 Exotech, 7 Exotech, 10 Exotech, 1 God of the Net, 2 Golden Ox, 4 Golden Ox, 6 Golden Ox, 3 Nitro, 4 Nitro, 1 Overlord, 1 Soul Killer, 3 Street Demon, 5 Street Demon, 7 Street Demon, 10 Street Demon, 3 Syndicate, 5 Syndicate, 7 Syndicate, 1 Virus, 2 A.M.P., 3 A.M.P., 4 A.M.P., 5 A.M.P., 2 Bastion, 4 Bastion, 6 Bastion, 2 Bruiser, 4 Bruiser, 6 Bruiser, 2 Dynamo, 3 Dynamo, 4 Dynamo, 2 Executioner, 3 Executioner, 4 Executioner, 5 Executioner, 2 Marksman, 4 Marksman, 2 Rapidfire, 4 Rapidfire, 6 Rapidfire, 2 Slayer, 4 Slayer, 6 Slayer, 2 Strategist, 3 Strategist, 4 Strategist, 5 Strategist, 2 Techie, 4 Techie, 6 Techie, 8 Techie, 2 Vanguard, 4 Vanguard, 6 Vanguard"
 
     model="gemini-2.0-flash"
 
@@ -93,16 +102,28 @@ def get_filters(prompt):
             "properties": {
                 "filter_1": {
                     "type": "string",
-                    "description": "A champion in tft. The champions consist of: Alistar, Annie, Aphelios, Aurora, Brand, Braum, Chogath, Darius, Draven, Dr. Mundo, Ekko, Elise, Fiddlesticks, Galio, Garen, Gragas, Graves, Illaoi, Jarvan IV, Jax, Jhin, Jinx, Kindred, Kobuko, Kogmaw, Leblanc, Leona, Mis Fortune, Mordekaiser, Morgana, Naafiri, Neeko, Nidalee, Poppy, Renekton, Rengar, Rhaast, Samira, Sejuani, Senna, Seraphine, Shaco, Shyvana, Skarner, Sylas, Twisted Fate, Urgot, Varus, vayne, Veigar, Vex, Vi, Viego, Xayah, Yuumi, Zac, Zed, Zeri, Ziggs, Zyra"
+                    "description": f"A champion or trait in tft. The champions consist of: {champions}. The traits consist of: {traits}"
+                },
+                "champion_tier_1": {
+                    "type":"string",
+                    "description": f"If the previous filter_1 includes a champions from this list: {champions}, the user can include a champion's star level if a number comes after a champion name. The options are 0 if nothing is specified, 1, 2, or 3."
                 },
                 "filter_2": {
                     "type": "string",
-                    "description": "If the user specifies another champion in tft use this filter, the same champion options apply. The champions consist of: Alistar, Annie, Aphelios, Aurora, Brand, Braum, Chogath, Darius, Draven, Dr. Mundo, Ekko, Elise, Fiddlesticks, Galio, Garen, Gragas, Graves, Illaoi, Jarvan IV, Jax, Jhin, Jinx, Kindred, Kobuko, Kogmaw, Leblanc, Leona, Mis Fortune, Mordekaiser, Morgana, Naafiri, Neeko, Nidalee, Poppy, Renekton, Rengar, Rhaast, Samira, Sejuani, Senna, Seraphine, Shaco, Shyvana, Skarner, Sylas, Twisted Fate, Urgot, Varus, vayne, Veigar, Vex, Vi, Viego, Xayah, Yuumi, Zac, Zed, Zeri, Ziggs, Zyra"
+                    "description": f"If the user specifies another champion or trait in tft use this filter, the same champion options apply. The champions consist of: {champions}. The traits consist of {traits}"
+                },
+                "champion_tier_2": {
+                    "type":"string",
+                    "description": f"If the previous filter_2 includes a champions from this list: {champions}, the user can include a champion's star level if a number comes after a champion name. The options are 0 if nothing is specified, 1, 2, or 3."
                 },
                 "filter_3": {
                     "type": "string",
-                    "description": "If the user specifies a third champion in tft use this filter, the same champion options apply. The champions consist of: Alistar, Annie, Aphelios, Aurora, Brand, Braum, Chogath, Darius, Draven, Dr. Mundo, Ekko, Elise, Fiddlesticks, Galio, Garen, Gragas, Graves, Illaoi, Jarvan IV, Jax, Jhin, Jinx, Kindred, Kobuko, Kogmaw, Leblanc, Leona, Mis Fortune, Mordekaiser, Morgana, Naafiri, Neeko, Nidalee, Poppy, Renekton, Rengar, Rhaast, Samira, Sejuani, Senna, Seraphine, Shaco, Shyvana, Skarner, Sylas, Twisted Fate, Urgot, Varus, vayne, Veigar, Vex, Vi, Viego, Xayah, Yuumi, Zac, Zed, Zeri, Ziggs, Zyra"
-                }
+                    "description": f"If the user specifies a third champion or trait in tft use this filter, the same champion options apply. The champions consist of: {champions}. The traits consist of {traits}"
+                },
+                "champion_tier_3": {
+                    "type":"string",
+                    "description": f"If the previous filter_3 includes a champions from this list: {champions}, the user can include a champion's star level if a number comes after a champion name. The options are 0 if nothing is specified, 1, 2, or 3."
+                },
             },
             "required": ["filter_1"]
         }
@@ -116,15 +137,18 @@ def get_filters(prompt):
         model=model,
         config=config,
     )
-
+    # total tokens is around 2.5k
     response = chat.send_message(prompt)
+    
     # print(response)
     if response.function_calls == None:
         return None
     else:
         filters = []
 
-        for champion in response.function_calls[0].args.values():
-            filters.append(champion)
+        for filter in response.function_calls[0].args.items():
+            filters.append(filter)
         
-        return filters
+        order = {'filter_1': 0, 'champion_tier_1': 1, "filter_2": 2, 'champion_tier_2':3, 'filter_3': 4, 'champion_tier_3':5}
+        sorted_filters = sorted(filters, key=lambda x: order.get(x[0], float('inf')))
+        return sorted_filters
--- src/tactics_simple_api.py
+++ src/tactics_simple_api.py
@@ -20,94 +20,149 @@ import requests
 
 base_url = "https://d3.tft.tools/explorer-data/15080/1/"
 
-trait_mapping = {'Overlord': 'TFT14_Overlord',
- 'Street Demon': 'TFT14_StreetDemon',
- 'Soul Killer': 'TFT14_ViegoUniqueTrait',
- 'Syndicate': 'TFT14_Mob',
- 'Techie': 'TFT14_Techie',
- 'Anima Squad': 'TFT14_AnimaSquad',
- 'BoomBot': 'TFT14_BallisTek',
- 'Virus': 'TFT14_Virus',
- 'Strategist': 'TFT14_Controller',
- 'Executioner': 'TFT14_Cutter',
- 'Marksman': 'TFT14_Marksman',
- 'Bastion': 'TFT14_Armorclad',
- 'Exotech': 'TFT14_EdgeRunner',
- 'A.M.P.': 'TFT14_Supercharge',
- 'Nitro': 'TFT14_HotRod',
- 'Cyberboss': 'TFT14_Cyberboss',
- 'Vanguard': 'TFT14_Vanguard',
- 'Divinicorp': 'TFT14_Divinicorp',
- 'Dynamo': 'TFT14_Thirsty',
- 'Rapidfire': 'TFT14_Swift',
- 'Cypher': 'TFT14_Suits',
- 'Slayer': 'TFT14_Strong',
- 'Golden Ox': 'TFT14_Immortal',
- 'God of the Net': 'TFT14_Netgod',
- 'Bruiser': 'TFT14_Bruiser'}
-champion_mapping = {'Varus': 'TFT14_Varus',
- 'Jarvan IV': 'TFT14_Jarvan',
- 'Dr. Mundo': 'TFT14_DrMundo',
- 'Mordekaiser': 'TFT14_Mordekaiser',
- 'Kobuko': 'TFT14_Kobuko',
- 'Shyvana': 'TFT14_Shyvana',
- 'Elise': 'TFT14_Elise',
- 'Illaoi': 'TFT14_Illaoi',
- 'Fiddlesticks': 'TFT14_Fiddlesticks',
- 'Naafiri': 'TFT14_Naafiri',
- 'Tibbers': 'TFT14_AnnieTibbers',
- 'Shaco': 'TFT14_Shaco',
- 'Sejuani': 'TFT14_Sejuani',
- 'T-43X': 'TFT14_SummonLevel4',
- 'Twisted Fate': 'TFT14_TwistedFate',
- 'R-080T': 'TFT14_SummonLevel2',
- 'Zeri': 'TFT14_Zeri',
- 'Skarner': 'TFT14_Skarner',
- 'Vex': 'TFT14_Vex',
- 'Neeko': 'TFT14_Neeko',
- 'Zac': 'TFT14_Zac',
- 'Brand': 'TFT14_Brand',
- 'Vayne': 'TFT14_Vayne',
- 'Senna': 'TFT14_Senna',
- 'Galio': 'TFT14_Galio',
- 'Samira': 'TFT14_Samira',
- 'Xayah': 'TFT14_Xayah',
- 'Ziggs': 'TFT14_Ziggs',
- 'Zed': 'TFT14_Zed',
- 'Annie': 'TFT14_Annie',
- 'Zyra': 'TFT14_Zyra',
- 'Yuumi': 'TFT14_Yuumi',
- 'Jhin': 'TFT14_Jhin',
- 'Graves': 'TFT14_Graves',
- 'Viego': 'TFT14_Viego',
- 'Braum': 'TFT14_Braum',
- 'Leona': 'TFT14_Leona',
- "Cho'Gath": 'TFT14_Chogath',
- 'Ekko': 'TFT14_Ekko',
- 'Sylas': 'TFT14_Sylas',
- 'Draven': 'TFT14_Draven',
- 'Kindred': 'TFT14_Kindred',
- 'Miss Fortune': 'TFT14_MissFortune',
- 'Urgot': 'TFT14_Urgot',
- 'Jax': 'TFT14_Jax',
- 'Rengar': 'TFT14_Rengar',
- 'Aurora': 'TFT14_Aurora',
- 'Nidalee': 'TFT14_NidaleeCougar',
- 'Poppy': 'TFT14_Poppy',
- 'LeBlanc': 'TFT14_LeBlanc',
- 'Vi': 'TFT14_Vi',
- 'Darius': 'TFT14_Darius',
- 'Morgana': 'TFT14_Morgana',
- 'Garen': 'TFT14_Garen',
- 'Gragas': 'TFT14_Gragas',
- 'Seraphine': 'TFT14_Seraphine',
- 'Jinx': 'TFT14_Jinx',
- 'Alistar': 'TFT14_Alistar',
- 'Renekton': 'TFT14_Renekton',
- 'Veigar': 'TFT14_Veigar',
- "Kog'Maw": 'TFT14_KogMaw',
- 'Aphelios': 'TFT14_Aphelios',
- 'Rhaast': 'TFT14_Rhaast'}
+trait_mapping = {
+    '3 Anima Squad': 'TFT14_AnimaSquad-1',
+    '5 Anima Squad': 'TFT14_AnimaSquad-2',
+    '7 Anima Squad': 'TFT14_AnimaSquad-3',
+    '10 Anima Squad': 'TFT14_AnimaSquad-4',
+    '2 BoomBot': 'TFT14_BallisTek-1',
+    '4 BoomBot': 'TFT14_BallisTek-2',
+    '6 BoomBot': 'TFT14_BallisTek-3',
+    '2 Cyberboss': 'TFT14_Cyberboss-1',
+    '3 Cyberboss': 'TFT14_Cyberboss-2',
+    '4 Cyberboss': 'TFT14_Cyberboss-3',
+    '3 Cypher': 'TFT14_Suits-1',
+    '4 Cypher': 'TFT14_Suits-2',
+    '5 Cypher': 'TFT14_Suits-3',
+    '1 Divinicorp': 'TFT14_Divinicorp-1',
+    '2 Divinicorp': 'TFT14_Divinicorp-2',
+    '3 Divinicorp': 'TFT14_Divinicorp-3',
+    '4 Divinicorp': 'TFT14_Divinicorp-4',
+    '5 Divinicorp': 'TFT14_Divinicorp-5',
+    '6 Divinicorp': 'TFT14_Divinicorp-6',
+    '7 Divinicorp': 'TFT14_Divinicorp-7',
+    '3 Exotech': 'TFT14_EdgeRunner-1',
+    '5 Exotech': 'TFT14_EdgeRunner-2',
+    '7 Exotech': 'TFT14_EdgeRunner-3',
+    '10 Exotech': 'TFT14_EdgeRunner-4',
+    '1 God of the Net': 'TFT14_Netgod-1',
+    '2 Golden Ox': 'TFT14_Immortal-1',
+    '4 Golden Ox': 'TFT14_Immortal-2',
+    '6 Golden Ox': 'TFT14_Immortal-3',
+    '3 Nitro': 'TFT14_HotRod-1',
+    '4 Nitro': 'TFT14_HotRod-2',
+    '1 Overlord': 'TFT14_Overlord-1',
+    '1 Soul Killer': 'TFT14_ViegoUniqueTrait-1',
+    '3 Street Demon': 'TFT14_StreetDemon-1',
+    '5 Street Demon': 'TFT14_StreetDemon-2',
+    '7 Street Demon': 'TFT14_StreetDemon-3',
+    '10 Street Demon': 'TFT14_StreetDemon-4',
+    '3 Syndicate': 'TFT14_Mob-1',
+    '5 Syndicate': 'TFT14_Mob-2',
+    '7 Syndicate': 'TFT14_Mob-3',
+    '1 Virus': 'TFT14_Virus-1',
+    '2 A.M.P.': 'TFT14_Supercharge-1',
+    '3 A.M.P.': 'TFT14_Supercharge-2',
+    '4 A.M.P.': 'TFT14_Supercharge-3',
+    '5 A.M.P.': 'TFT14_Supercharge-4',
+    '2 Bastion': 'TFT14_Armorclad-1',
+    '4 Bastion': 'TFT14_Armorclad-2',
+    '6 Bastion': 'TFT14_Armorclad-3',
+    '2 Bruiser': 'TFT14_Bruiser-1',
+    '4 Bruiser': 'TFT14_Bruiser-2',
+    '6 Bruiser': 'TFT14_Bruiser-3',
+    '2 Dynamo': 'TFT14_Thirsty-1',
+    '3 Dynamo': 'TFT14_Thirsty-2',
+    '4 Dynamo': 'TFT14_Thirsty-3',
+    '2 Executioner': 'TFT14_Cutter-1',
+    '3 Executioner': 'TFT14_Cutter-2',
+    '4 Executioner': 'TFT14_Cutter-3',
+    '5 Executioner': 'TFT14_Cutter-4',
+    '2 Marksman': 'TFT14_Marksman-1',
+    '4 Marksman': 'TFT14_Marksman-2',
+    '2 Rapidfire': 'TFT14_Swift-1',
+    '4 Rapidfire': 'TFT14_Swift-2',
+    '6 Rapidfire': 'TFT14_Swift-3',
+    '2 Slayer': 'TFT14_Strong-1',
+    '4 Slayer': 'TFT14_Strong-2',
+    '6 Slayer': 'TFT14_Strong-3',
+    '2 Strategist': 'TFT14_Controller-1',
+    '3 Strategist': 'TFT14_Controller-2',
+    '4 Strategist': 'TFT14_Controller-3',
+    '5 Strategist': 'TFT14_Controller-4',
+    '2 Techie': 'TFT14_Techie-1',
+    '4 Techie': 'TFT14_Techie-2',
+    '6 Techie': 'TFT14_Techie-3',
+    '8 Techie': 'TFT14_Techie-4',
+    '2 Vanguard': 'TFT14_Vanguard-1',
+    '4 Vanguard': 'TFT14_Vanguard-2',
+    '6 Vanguard': 'TFT14_Vanguard-3'
+    }
+champion_mapping = {
+    'Varus': 'TFT14_Varus',
+    'Jarvan IV': 'TFT14_Jarvan',
+    'Dr. Mundo': 'TFT14_DrMundo',
+    'Mordekaiser': 'TFT14_Mordekaiser',
+    'Kobuko': 'TFT14_Kobuko',
+    'Shyvana': 'TFT14_Shyvana',
+    'Elise': 'TFT14_Elise',
+    'Illaoi': 'TFT14_Illaoi',
+    'Fiddlesticks': 'TFT14_Fiddlesticks',
+    'Naafiri': 'TFT14_Naafiri',
+    'Tibbers': 'TFT14_AnnieTibbers',
+    'Shaco': 'TFT14_Shaco',
+    'Sejuani': 'TFT14_Sejuani',
+    'T-43X': 'TFT14_SummonLevel4',
+    'Twisted Fate': 'TFT14_TwistedFate',
+    'R-080T': 'TFT14_SummonLevel2',
+    'Zeri': 'TFT14_Zeri',
+    'Skarner': 'TFT14_Skarner',
+    'Vex': 'TFT14_Vex',
+    'Neeko': 'TFT14_Neeko',
+    'Zac': 'TFT14_Zac',
+    'Brand': 'TFT14_Brand',
+    'Vayne': 'TFT14_Vayne',
+    'Senna': 'TFT14_Senna',
+    'Galio': 'TFT14_Galio',
+    'Samira': 'TFT14_Samira',
+    'Xayah': 'TFT14_Xayah',
+    'Ziggs': 'TFT14_Ziggs',
+    'Zed': 'TFT14_Zed',
+    'Annie': 'TFT14_Annie',
+    'Zyra': 'TFT14_Zyra',
+    'Yuumi': 'TFT14_Yuumi',
+    'Jhin': 'TFT14_Jhin',
+    'Graves': 'TFT14_Graves',
+    'Viego': 'TFT14_Viego',
+    'Braum': 'TFT14_Braum',
+    'Leona': 'TFT14_Leona',
+    "Cho'Gath": 'TFT14_Chogath',
+    'Ekko': 'TFT14_Ekko',
+    'Sylas': 'TFT14_Sylas',
+    'Draven': 'TFT14_Draven',
+    'Kindred': 'TFT14_Kindred',
+    'Miss Fortune': 'TFT14_MissFortune',
+    'Urgot': 'TFT14_Urgot',
+    'Jax': 'TFT14_Jax',
+    'Rengar': 'TFT14_Rengar',
+    'Aurora': 'TFT14_Aurora',
+    'Nidalee': 'TFT14_NidaleeCougar',
+    'Poppy': 'TFT14_Poppy',
+    'LeBlanc': 'TFT14_LeBlanc',
+    'Vi': 'TFT14_Vi',
+    'Darius': 'TFT14_Darius',
+    'Morgana': 'TFT14_Morgana',
+    'Garen': 'TFT14_Garen',
+    'Gragas': 'TFT14_Gragas',
+    'Seraphine': 'TFT14_Seraphine',
+    'Jinx': 'TFT14_Jinx',
+    'Alistar': 'TFT14_Alistar',
+    'Renekton': 'TFT14_Renekton',
+    'Veigar': 'TFT14_Veigar',
+    "Kog'Maw": 'TFT14_KogMaw',
+    'Aphelios': 'TFT14_Aphelios',
+    'Rhaast': 'TFT14_Rhaast'}
+
 
 champion_mapping_reverse = {v: k for k, v in champion_mapping.items()}
 trait_mapping_reverse = {v: k for k, v in trait_mapping.items()}
@@ -123,6 +178,7 @@ def _post_process_champion(data, top_n=10):
             "win_rate": round(row[1]["won"] / row[1]["count"], 2),
             "top_4_rate": round(row[1]["top4"] / row[1]["count"], 2),
             "average_placement": round(row[1]["place"] / row[1]["count"], 2),
+            "delta": round(row[1]['delta'], 2)
         })
 
     # Sort by count
@@ -134,13 +190,14 @@ def _post_process_trait(data, top_n=10):
     output = []
     for row in data:
         # TODO: add tier to the trait
-        name = row[0]
+        name = row[0] + f"-{str(row[1])}"
         output.append({
-            "name": trait_mapping_reverse[name] + " tier " + str(row[1]),
+            "name": trait_mapping_reverse[name],
             "count": row[2]["count"],
             "win_rate": round(row[2]["won"] / row[2]["count"], 2),
             "top_4_rate": round(row[2]["top4"] / row[2]["count"], 2),
             "average_placement": round(row[2]["place"] / row[2]["count"], 2),
+            "delta": round(row[2]['delta'], 2)
         })
 
     # Sort by count
@@ -149,9 +206,9 @@ def _post_process_trait(data, top_n=10):
 
 def _to_string(data):
     # TODO: can optimize this format
-    output = "Name (number of games) - win rate - top 4 rate - average placement\n"
+    output = "Name, number of games, win rate, top 4 rate, average placement, delta\n"
     for item in data:
-        output += f"{item['name']} ({item['count']}) - {item['win_rate']} - {item['top_4_rate']} - {item['average_placement']}\n"
+        output += f"{item['name']}, {item['count']}, {item['win_rate']}, {item['top_4_rate']}, {item['average_placement']}, {item['delta']}\n"
     return output
 
 def get_champion_data(filter_list):
@@ -159,16 +216,30 @@ def get_champion_data(filter_list):
     # Champions are u-<champion>-<tier>
     # Traits are t-<trait>-<tier>
     filter_string = ""
-    for filter in filter_list:
-
-        filter_name = filter["filter"]
-        filter_tier = filter["tier"]
-        
-        if filter_name in champion_mapping:
-            filter_string += f"u-{champion_mapping[filter_name]}-{filter_tier}/"
-        elif filter_name in trait_mapping:
-            filter_string += f"t-{trait_mapping[filter_name]}-{filter_tier}/"
     
+    # for filter in filter_list:
+
+    #     filter_name = filter["filter"]
+    #     filter_tier = filter["tier"]
+        
+    #     if filter_name in champion_mapping:
+    #         filter_string += f"u-{champion_mapping[filter_name]}-{filter_tier}/"
+    #     elif filter_name in trait_mapping:
+    #         filter_string += f"t-{trait_mapping[filter_name]}-{filter_tier}/"
+
+    for i in range(len(filter_list)):
+        if filter_list[i][1] in champion_mapping:
+            champion = filter_list[i][1]
+            filter_string += f"u-{champion_mapping[champion]}"
+            if i + 1 < len(filter_list) and 'champion_tier' not in filter_list[i+1][0]:
+                filter_string += f"-0/"
+        elif 'champion_tier' in filter_list[i][0]:
+            champion_tier = filter_list[i][1]
+            filter_string += f"-{champion_tier}/"
+        else:
+            filter_name = filter_list[i][1]
+            filter_string += f"t-{trait_mapping[filter_name]}"
+
     url = (base_url + filter_string).rstrip('/')
     response = requests.get(url)
     response_json = response.json()
@@ -182,13 +253,24 @@ def get_trait_data(filter_list):
     filter_string = ""
     for filter in filter_list:
         
-        filter_name = filter["filter"]
-        filter_tier = filter["tier"]
+        if filter[1] in champion_mapping:
+            champion = filter[1]
+            filter_string += f"u-{champion_mapping[champion]}"
+        elif 'champion_tier' in filter[0]:
+            champion_tier = filter[1]
+            filter_string += f"-{champion_tier}/"
+        else:
+            filter_name = filter[1]
+            filter_string += f"t-{trait_mapping[filter_name]}/"
+
+        # filter_name = filter["filter"]
+        # filter_tier = filter["tier"]
         
-        if filter_name in champion_mapping:
-            filter_string += f"u-{champion_mapping[filter_name]}-{filter_tier}/"
-        elif filter_name in trait_mapping:
-            filter_string += f"t-{trait_mapping[filter_name]}-{filter_tier}/"
+        # if filter_name in champion_mapping:
+        #     filter_string += f"u-{champion_mapping[filter_name]}-{filter_tier}/"
+        # elif filter_name in trait_mapping:
+        #     filter_string += f"t-{trait_mapping[filter_name]}-{filter_tier}/"
+            
     url = (base_url + filter_string).rstrip('/')
     response = requests.get(url)
     response_json = response.json()
